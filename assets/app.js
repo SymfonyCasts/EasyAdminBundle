912faa9cf3d9a38b4db9beea494d268b8c57fcf2
bocharsky-bw <bocharsky.bw@gmail.com>
2021-07-14 19:20:57 +0300

    Update webpack encore recipe

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
