0c14040f47084bc1e51fc850db4441f3ffbe8d5e
bocharsky-bw <bocharsky.bw@gmail.com>
2021-07-16 13:08:00 +0300

    Implement User login

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,22 +2,49 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Question;
+use App\Entity\User;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadQuestions();
+        $this->loadUsers($manager);
+
+        $manager->flush();
+    }
+
+    private function loadQuestions()
     {
         QuestionFactory::new()->createMany(20);
 
         QuestionFactory::new()
             ->unpublished()
-            ->createMany(5)
-        ;
+            ->createMany(5);
+    }
 
-        $manager->flush();
+    private function loadUsers(ObjectManager $manager)
+    {
+        $adminUser = new User();
+        $adminUser->setEmail('admin@symfonycasts.com');
+        $adminUser->setRoles([
+            'ROLE_SUPER_ADMIN',
+        ]);
+        $hashedPassword = $this->passwordHasher
+            ->hashPassword($adminUser, 'adminpass');
+        $adminUser->setPassword($hashedPassword);
+
+        $manager->persist($adminUser);
     }
 }
