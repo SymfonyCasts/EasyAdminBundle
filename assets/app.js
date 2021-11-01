fc7a076660c3a725911d921a63ca91d143775f1a
bocharsky-bw <bocharsky.bw@gmail.com>
2021-11-01 22:23:36 +0200

    Copy assets from Symfony 5 tutorial

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,9 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
+// activates collapse functionality
+import { Collapse } from 'bootstrap';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 
