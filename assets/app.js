76539f19477aa82e322aa82ac1c2297cdfd037ca
bocharsky-bw <bocharsky.bw@gmail.com>
2021-11-16 19:16:23 +0200

    Get rid of the JS code for votes

--- assets/app.js
+++ assets/app.js
@@ -13,22 +13,3 @@ import './bootstrap';
 
 // activates collapse functionality
 import { Collapse } from 'bootstrap';
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
-
-/**
- * Simple (ugly) code to handle the comment vote up/down
- */
-var $container = $('.js-vote-arrows');
-$container.find('button').on('click', function(e) {
-    e.preventDefault();
-    var $button = $(e.currentTarget);
-
-    $.ajax({
-        url: '/comments/10/vote/'+$button.val(),
-        method: 'POST'
-    }).then(function(data) {
-        $container.find('.js-vote-total').text(data.votes);
-    });
-});
