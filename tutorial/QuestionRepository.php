ac8984c72390c4dcb549613a481898a6a1f8514e
bocharsky-bw <bocharsky.bw@gmail.com>
2021-11-17 19:58:44 +0200

    Add Question repo code to a tutorial/ dir

---
+++ tutorial/QuestionRepository.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Question;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Question|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Question|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Question[]    findAll()
+ * @method Question[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class QuestionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Question::class);
+    }
+
+    public function findLatest(): array
+    {
+        return $this->createQueryBuilder('question')
+            ->orderBy('question.createdAt', 'DESC')
+            ->setMaxResults(3)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findTopVoted(): array
+    {
+        return $this->createQueryBuilder('question')
+            ->orderBy('question.votes', 'DESC')
+            ->setMaxResults(5)
+            ->getQuery()
+            ->getResult();
+    }
+
+     /**
+      * @return Question[] Returns an array of Question objects
+      */
+    public function findAllApprovedOrderedByNewest()
+    {
+        return $this->addIsApprovedQueryBuilder()
+            ->orderBy('q.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    private function addIsApprovedQueryBuilder(QueryBuilder $qb = null): QueryBuilder
+    {
+        return $this->getOrCreateQueryBuilder($qb)
+            ->andWhere('q.isApproved = 1');
+    }
+
+    private function getOrCreateQueryBuilder(QueryBuilder $qb = null): QueryBuilder
+    {
+        return $qb ?: $this->createQueryBuilder('q');
+    }
+
+    /*
+    public function findOneBySomeField($value): ?Question
+    {
+        return $this->createQueryBuilder('q')
+            ->andWhere('q.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
