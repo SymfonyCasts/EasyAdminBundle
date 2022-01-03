d243004afcb986c5072fce6342a53cb6dc6cda4a
Ryan Weaver <ryan@thatsquality.com>
2022-01-03 14:49:11 -0500

    escaping user table name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
